9f10f513a1e160ca716e3aac4f8cf959a903a32c
Anatolii <tol2000@users.noreply.github.com>
2021-11-11 12:50:45 +0200

    Update bin.py

--- bin.py
+++ bin.py
@@ -6,12 +6,14 @@ if __name__ == '__main__':
     alphabet = ['0', '1']
     min_word_len = 1
     max_word_len = 32
-    start_from_str = '10101110100101110000001'
+    start_from_str = '11111100110000000110110'
     start_from = [x for x in start_from_str]
 
-    words = ListAllWords(
-        alphabet, min_word_len, max_word_len, reduce_to_str=True
-        , start_from=start_from
-    )
-    for word_a in words:
-        print(word_a)
+    with open('f.txt','w') as f:
+        words = ListAllWords(
+            alphabet, min_word_len, max_word_len, reduce_to_str=True
+            , start_from=start_from
+        )
+        for word_a in words:
+            print(word_a)
+            # f.write(word_a+"\n")
